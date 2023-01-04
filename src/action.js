1b09f813cc70c28ffcb5d1fdf28b15fbb940591c
Smit Vasoya <smitvtridhyatech@gmail.com>
2023-01-04 14:43:34 +0530

    second commit

--- src/action.js
+++ src/action.js
@@ -1,5 +1,18 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 async function run() {
-  console.log("Hello World!!");
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const oktokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await oktokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for Submitting the pull request",
+  });
 }
 
 run();
