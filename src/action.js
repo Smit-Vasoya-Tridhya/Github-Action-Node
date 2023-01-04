8015a3cff9cbb536c300dc477a7d6ca545ad91a6
Smit Vasoya <smitvtridhyatech@gmail.com>
2023-01-04 17:04:04 +0530

    third

--- src/action.js
+++ src/action.js
@@ -1,9 +1,17 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fetch = require("node-fetch");
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const TENOR_TOKEN = core.getInput("TENOR_TOKEN");
   const oktokit = github.getOctokit(GITHUB_TOKEN);
+
+  const randomPos = Math.round(Math.round() * 1000);
+  const url = `https://g.tenor.com/v1/search?q=excited&pos=${randomPos}&key=${TENOR_TOKEN}&limit=1&media_filter=minimal&contentfilter=high`;
+  const response = await fetch(url);
+  const { results } = await response.json();
+  const gifUrl = results[0].media[0].tinygif.url;
 
   const { context = {} } = github;
   const { pull_request } = context.payload;
@@ -11,7 +19,7 @@ async function run() {
   await oktokit.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: "Thank you for Submitting the pull request",
+    body: `Thank you for Submitting the pull request  <img src="${gifUrl}" alt="image" /> `,
   });
 }
 
